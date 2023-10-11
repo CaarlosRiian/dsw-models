f33bcf0ba745fa3f0e5c48da8a2e8af915c96f18
Rian <rlina1785@gmail.com>
2023-10-11 12:35:30 -0300

    relacionamentos

--- atv/admin.py
+++ atv/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Disco
+from .models import Disco, Selo, Artista
 
 admin.site.register(Disco)
+admin.site.register(Selo)
+admin.site.register(Artista)
--- atv/models.py
+++ atv/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 
+class Artista(models.Model):
+    nome = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome
+
+class Selo(models.Model):
+    nome_produtora = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.nome_produtora
+
 class Disco(models.Model):
     nome = models.CharField(max_length=50)
     descricao = models.CharField(max_length=200)
-    nome_produtora = models.CharField(max_length=50)
+    selo = models.ForeignKey(Selo, on_delete=models.CASCADE, blank=True, null=True)
     ano = models.DateTimeField("Data Publicada")
     pais = models.CharField(max_length=50) 
     genero = models.CharField(max_length=50)
     quantidade = models.IntegerField()
+    artistas = models.ManyToManyField(Artista, related_name="discos")
 
-# Toda vez que mudar algo nos models
-# você tem que atualziar o banco de dados
-# python manage.py makemigrations -> mandar atualizações
-# python manage.py migrate -> atualizar
+    def __str__(self):
+        return self.nome
